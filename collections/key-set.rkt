6a01b9036e0acf1335a5d1c484ed4c2a164ab946
Jesper Wendel Devantier <j@asosio.com>
2016-01-17 22:45:20 +0100

    key-set: added set-handling capability.

--- collections/key-set.rkt
+++ collections/key-set.rkt
@@ -2,29 +2,27 @@
 
 (require racket/generic
          (only-in racket/function negate conjoin)
-         (only-in racket/dict dict-mutable? dict? dict-set dict-set!)
-         (prefix-in dict- cameron/collections/dict)
-         (prefix-in set- cameron/collections/set)
+         (only-in racket/dict dict-mutable?)
+         (only-in racket/set set mutable-set set-mutable?)
+         racket/require
+         (prefix-in c/ (multi-in cameron/collections [dict set]))
          cameron/defs)
 
 (provide
  gen:key-set key-set? key-set/c
  key-set)
 
-(defn -dict-set! [d & args]
-  (apply dict-set! d args)
-  d)
-
 (define-generics key-set
-  (key-set key-set k v)
+  (key-set key-set k . rst)
   #:defaults
-  ([(conjoin dict? immutable?)
-    (def key-set dict-assoc)]
-   ;; regular list of pairs
-   [(conjoin dict? list?) 
-    (def key-set dict-assoc)]
-   [dict-mutable?
-    (def key-set dict-assoc!)]))
+  ([c/dict-immutable?
+    (def key-set c/dict-assoc)]
+   [c/dict-mutable?
+    (def key-set c/dict-assoc!)]
+   [c/set-immutable?
+    (def key-set c/set-assoc)]
+   [c/set-mutable?
+    (def key-set c/set-assoc!)]))
 
 ;; TODO - missing multiple assignment tests (if wanted)
 (module+ test
@@ -49,14 +47,100 @@
            (check-equal? (dict-ref d 'b #f) #f)
            (check-equal? (dict-ref d 'b #t) #t)))
 
+  ;; key-set - immutable dicts
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (test-case
-      "key-set - immutable dict tests"
-    (local [(def d (make-immutable-hash '((a . 1) (c . 3))))]
-           ;; ensure dict value is set and orig values retained
-           (check-equal? (dict-ref (key-set d 'b 2) 'a) 1)
-           (check-equal? (dict-ref (key-set d 'b 2) 'b) 2)
-           (check-equal? (dict-ref (key-set d 'b 2) 'c) 3)
+      "key-set - immutable dicts - add an entry"
+    (local [(def ih (make-immutable-hash '((a . 1) (d . 4))))]
+           ;; add an entry
+           (let ([ret-dict (key-set ih 'e 10)])
+             (check-equal? ret-dict (make-immutable-hash '((a . 1) (d . 4) (e . 10)))))
+           ;; ensure modification is not in-place
+           (check-equal? ih (make-immutable-hash '((a . 1) (d . 4))))))
 
-           ;; old dict should not be altered
-           (check-equal? (dict-ref d 'b #f) #f)
-           (check-equal? (dict-ref d 'b #t) #t))))
+  (test-case
+      "key-set - immutable dicts - overwrite an entry"
+    (local [(def ih (make-immutable-hash '((a . 1) (d . 4))))]
+           ;; add an entry
+           (let ([ret-dict (key-set ih 'a 10)])
+             (check-equal? ret-dict (make-immutable-hash '((a . 10) (d . 4)))))
+           ;; ensure modification is not in-place
+           (check-equal? ih (make-immutable-hash '((a . 1) (d . 4))))))
+
+  (test-case
+      "key-set - immutable dicts - add several entries"
+    (local [(def ih (make-immutable-hash '((a . 1) (d . 4))))]
+           ;; add several entries
+           (let ([ret-dict (key-set ih 'd 7 'e 10 'f 25)])
+             (check-equal? ret-dict (make-immutable-hash '((a . 1) (d . 7) (e . 10) (f . 25))))
+             ;; ensure modification is in-place
+             (check-equal? ih (make-immutable-hash '((a . 1) (d . 4)))))))
+
+  ;; key-set - mutable dicts
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (test-case
+      "key-set - mutable dicts - add an entry"
+    (local [(def mh (make-hash '((a . 1) (d . 4))))]
+           ;; add an entry
+           (let ([ret-dict (key-set mh 'e 10)])
+             (check-equal? ret-dict (make-hash '((a . 1) (d . 4) (e . 10))))
+             ;; ensure modification is in-place
+             (check-equal? mh ret-dict))))
+
+  (test-case
+      "key-set - mutable dicts - overwrite an entry"
+    (local [(def mh (make-hash '((a . 1) (d . 4))))]
+           ;; overwrite an entry
+           (let ([ret-dict (key-set mh 'a 10)])
+             (check-equal? ret-dict (make-hash '((a . 10) (d . 4))))
+             ;; ensure modification is in-place
+             (check-equal? mh ret-dict))))
+
+  (test-case
+      "key-set - mutable dicts - add several entries"
+    (local [(def mh (make-hash '((a . 1) (d . 4))))]
+           ;; add several entries
+           (let ([ret-dict (key-set mh 'd 7 'e 10 'f 25)])
+             (check-equal? ret-dict (make-hash '((a . 1) (d . 7) (e . 10) (f . 25))))
+             ;; ensure modification is in-place
+             (check-equal? mh ret-dict))))
+
+  ;; key-set - immutable sets
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (test-case
+      "key-set - immutable sets - add an entry"
+    (local [(def is (set 1 3 5))]
+           ;; add an entry
+           (let ([ret-set (key-set is 4)])
+             (check-equal? ret-set (set 4 1 3 5)))
+           ;; ensure modification is not in-place
+           (check-equal? is (set 1 3 5))))
+
+  (test-case
+      "key-set - immutable sets - add several entries"
+    (local [(def is (set 1 3 5))]
+           ;; add an entry
+           (let ([ret-set (key-set is 4 10 15)])
+             (check-equal? ret-set (set 4 1 3 5 10 15)))
+           ;; ensure modification is not in-place
+           (check-equal? is (set 1 3 5))))
+
+  ;; key-set - mutable set
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (test-case
+      "key-set! - mutable sets - add an entry"
+    (local [(def is (mutable-set 1 3 5))]
+           ;; add an entry
+           (let ([ret-set (key-set is 4)])
+             (check-equal? ret-set (mutable-set 4 1 3 5 )))
+           ;; ensure modification is in-place
+           (check-equal? is (mutable-set 1 3 5 4))))
+
+  (test-case
+      "key-set! - mutable sets - add several entries"
+    (local [(def is (mutable-set 1 3 5))]
+           ;; add an entry
+           (let ([ret-set (key-set is 4 10 15)])
+             (check-equal? ret-set (mutable-set 4 1 3 5 10 15)))
+           ;; ensure modification is in-place
+           (check-equal? is (mutable-set 1 3 5 4 10 15)))))
