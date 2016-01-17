2e9c713bf1b45fcc2598764d266993e88eb38ffa
Jesper Wendel Devantier <j@asosio.com>
2016-01-17 22:44:40 +0100

    set: prefixed functions with set-, overwrite set-immutable?

--- collections/set.rkt
+++ collections/set.rkt
@@ -2,56 +2,70 @@
 
 (require
  racket/contract
+ (only-in racket/function negate conjoin)
  (only-in racket/list first rest)
- (only-in racket/set set? set-mutable? set-add set-add! set-remove set-remove!)
+ (prefix-in b: (only-in racket/set set? set-mutable? set-add set-add! set-remove set-remove!))
  cameron/defs)
 
 ;; TODO - can remove the explicit (s k & ks) setup (case-fn matching solves the case of zero args)
 (provide
+ set-immutable? set-immutable/c
+ set-mutable? set-mutable/c
  (contract-out
-  [assoc   (->* (set? any/c) () #:rest (listof any/c) set?)]
-  [assoc!  (->* (set-mutable? any/c) () #:rest (listof any/c) set-mutable?)]
-  [dissoc  (->* (set? any/c) () #:rest (listof any/c) set?)]
-  [dissoc! (->* (set-mutable? any/c) () #:rest (listof any/c) set-mutable?)]))
+  [set-assoc   (->* (b:set? any/c) () #:rest (listof any/c) b:set?)]
+  [set-assoc!  (->* (b:set-mutable? any/c) () #:rest (listof any/c) b:set-mutable?)]
+  [set-dissoc  (->* (b:set? any/c) () #:rest (listof any/c) b:set?)]
+  [set-dissoc! (->* (b:set-mutable? any/c) () #:rest (listof any/c) b:set-mutable?)]))
 
-(def assoc
+(def set-immutable?
+  (conjoin b:set? (negate b:set-mutable?)))
+
+(def set-immutable/c
+  (flat-named-contract 'immutable-set set-immutable?))
+
+(def set-mutable? b:set-mutable?)
+
+(def set-mutable/c
+  (flat-named-contract 'mutable-set set-mutable?))
+
+(def set-assoc
   (case-fn
-   [(s k) (set-add s k)]
+   [(s k) (b:set-add s k)]
    [(s k & ks)
-    (let loop ([ks ks] [s (set-add s k)])
+    (let loop ([ks ks] [s (b:set-add s k)])
       (if (equal? '() ks)
           s
-          (loop (rest ks) (set-add s (first ks)))))]))
+          (loop (rest ks) (b:set-add s (first ks)))))]))
 
-(def assoc!
+(def set-assoc!
   (case-fn
-   [(s k) (begin (set-add! s k) s)]
+   [(s k) (begin (b:set-add! s k) s)]
    [(s k & ks)
-    (set-add! s k)
+    (b:set-add! s k)
     (let loop ([ks ks])
       (if (equal? '() ks)
           s
-          (begin (set-add! s (first ks))
+          (begin (b:set-add! s (first ks))
                  (loop (rest ks)))))]))
 
-(def dissoc
+(def set-dissoc
   (case-fn
-   [(s k) (set-remove s k)]
+   [(s k) (b:set-remove s k)]
    [(s k & ks)
-    (let loop ([ks ks] [s (set-remove s k)])
+    (let loop ([ks ks] [s (b:set-remove s k)])
       (if (equal? '() ks)
           s
-          (loop (rest ks) (set-remove s (first ks)))))]))
+          (loop (rest ks) (b:set-remove s (first ks)))))]))
 
-(def dissoc!
+(def set-dissoc!
   (case-fn
-   [(s k) (begin (set-remove! s k) s)]
+   [(s k) (begin (b:set-remove! s k) s)]
    [(s k & ks)
-    (set-remove! s k)
+    (b:set-remove! s k)
     (let loop ([ks ks])
       (if (equal? '() ks)
           s
-          (begin (set-remove! s (first ks))
+          (begin (b:set-remove! s (first ks))
                  (loop (rest ks)))))]))
 
 (module+ test
@@ -60,73 +74,73 @@
            racket/set)
 
   (test-case
-      "assoc - immutable sets - add an entry"
+      "set-assoc - immutable sets - add an entry"
     (local [(def is (set 1 3 5))]
            ;; add an entry
-           (let ([ret-set (assoc is 4)])
+           (let ([ret-set (set-assoc is 4)])
              (check-equal? ret-set (set 4 1 3 5)))
            ;; ensure modification is not in-place
            (check-equal? is (set 1 3 5))))
 
   (test-case
-      "assoc - immutable sets - add several entries"
+      "set-assoc - immutable sets - add several entries"
     (local [(def is (set 1 3 5))]
            ;; add an entry
-           (let ([ret-set (assoc is 4 10 15)])
+           (let ([ret-set (set-assoc is 4 10 15)])
              (check-equal? ret-set (set 4 1 3 5 10 15)))
            ;; ensure modification is not in-place
            (check-equal? is (set 1 3 5))))
 
   (test-case
-      "assoc! - mutable sets - add an entry"
+      "set-assoc! - mutable sets - add an entry"
     (local [(def is (mutable-set 1 3 5))]
            ;; add an entry
-           (let ([ret-set (assoc! is 4)])
+           (let ([ret-set (set-assoc! is 4)])
              (check-equal? ret-set (mutable-set 4 1 3 5 )))
            ;; ensure modification is in-place
            (check-equal? is (mutable-set 1 3 5 4))))
 
   (test-case
-      "assoc! - mutable sets - add several entries"
+      "set-assoc! - mutable sets - add several entries"
     (local [(def is (mutable-set 1 3 5))]
            ;; add an entry
-           (let ([ret-set (assoc! is 4 10 15)])
+           (let ([ret-set (set-assoc! is 4 10 15)])
              (check-equal? ret-set (mutable-set 4 1 3 5 10 15)))
            ;; ensure modification is in-place
            (check-equal? is (mutable-set 1 3 5 4 10 15))))
 
   (test-case
-      "dissoc - immutable sets - remove an entry"
+      "set-dissoc - immutable sets - remove an entry"
     (local [(def is (set 1 3 5))]
            ;; remove an entry
-           (let ([ret-set (dissoc is 3)])
+           (let ([ret-set (set-dissoc is 3)])
              (check-equal? ret-set (set 1 5)))
            ;; ensure modification is not in-place
            (check-equal? is (set 1 3 5))))
 
   (test-case
-      "dissoc - immutable sets - remove several entries"
+      "set-dissoc - immutable sets - remove several entries"
     (local [(def is (set 1 3 5))]
            ;; remove an entry
-           (let ([ret-set (dissoc is 1 5)])
+           (let ([ret-set (set-dissoc is 1 5)])
              (check-equal? ret-set (set 3)))
            ;; ensure modification is not in-place
            (check-equal? is (set 1 3 5))))
 
   (test-case
-      "dissoc! - mutable sets - remove an entry"
+      "set-dissoc! - mutable sets - remove an entry"
     (local [(def is (mutable-set 4 1 3 5 10 15))]
            ;; remove an entry
-           (let ([ret-set (dissoc! is 5)])
+           (let ([ret-set (set-dissoc! is 5)])
              (check-equal? ret-set (mutable-set 4 1 3 10 15)))
            ;; ensure modification is in-place
            (check-equal? is (mutable-set 1 3 4 10 15))))
 
   (test-case
-      "dissoc! - mutable sets - remove several entries"
+      "set-dissoc! - mutable sets - remove several entries"
     (local [(def is (mutable-set 4 1 3 5 10 15))]
            ;; remove an entry
-           (let ([ret-set (dissoc! is 4 10 15)])
+           (let ([ret-set (set-dissoc! is 4 10 15)])
              (check-equal? ret-set (mutable-set 1 3 5)))
            ;; ensure modification is in-place
            (check-equal? is (mutable-set 1 3 5)))))
