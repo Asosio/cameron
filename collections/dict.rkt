c91542a181dcb1836cb73ff8741d532ce9bc6fe9
Jesper Wendel Devantier <j@asosio.com>
2016-01-17 22:41:23 +0100

    dict: prefix fns with dict-, override dict-mutable?
    We prefix functions with dict so that one can require multiple
       cameron datastructure modules and not have functions overwrite
       one another (e.g. cameron/collections/{dict,set}).
    
    dict-mutable? was overwritten because the standard implementation
    yields a contract error if a non-dict is passed.
    - this *seems* counter-intuitive (isn't that what contracts are for?).
    The overwritten form is used in key-set (and eventually key-rm) to
    ensure branching doesn't yield aforementioned contract error.

--- collections/dict.rkt
+++ collections/dict.rkt
@@ -3,28 +3,40 @@
 (require
  (only-in racket/function negate conjoin)
  (only-in racket/list first rest)
+ (prefix-in b: (only-in racket/dict dict-mutable?))
  racket/contract
  cameron/defs
  racket/dict)
 
 (provide
- dict-immutable?
+ dict-immutable? dict-immutable/c
+ dict-mutable? dict-mutable/c
  (contract-out
-  [assoc   (->* (dict? any/c any/c) () #:rest (listof any/c) dict-immutable?)]
-  [assoc!  (->* (dict-mutable? any/c any/c) () #:rest (listof any/c) dict-mutable?)]
-  [dissoc  (->* (dict? any/c) () #:rest (listof any/c) dict?)]
-  [dissoc! (->* (dict-mutable? any/c) () #:rest (listof any/c) dict-mutable?)]))
+  [dict-assoc   (->* (dict? any/c any/c) () #:rest (listof any/c) dict-immutable?)]
+  [dict-assoc!  (->* (dict-mutable? any/c any/c) () #:rest (listof any/c) dict-mutable?)]
+  [dict-dissoc  (->* (dict? any/c) () #:rest (listof any/c) dict?)]
+  [dict-dissoc! (->* (dict-mutable? any/c) () #:rest (listof any/c) dict-mutable?)]))
 
 (def dict-immutable?
-  (conjoin (negate dict-mutable?) dict?))
+  (conjoin dict? (negate b:dict-mutable?)))
 
-(def assoc
+(def dict-immutable/c
+  (flat-named-contract 'immutable-dict dict-immutable?))
+
+;; if we don't do this, we'll get a contract violation
+(def dict-mutable?
+  (conjoin dict? b:dict-mutable?))
+
+(def dict-mutable/c
+  (flat-named-contract 'mutable-dict b:dict-mutable?))
+
+(def dict-assoc
   (case-fn
    [(d k v) (dict-set d k v)]
    [(d & kv-args)
     (when (positive? (modulo (length kv-args) 2))
       (raise-arguments-error
-       'assoc
+       'dict-assoc
        "uneven number of expressions. kv-args must come in <key> <val> pairs."
        "kv-args" kv-args))
     (let loop ([kw kv-args] [d d])
@@ -32,18 +44,18 @@
           d
           (loop (cddr kw) (dict-set d (car kw) (cadr kw)))))]))
 
-(def assoc!
+(def dict-assoc!
   (case-fn
    [(d k v) (begin (dict-set! d k v) d)]
    [(d & kv-args)
     (when (positive? (modulo (length kv-args) 2))
       (raise-arguments-error
-       'assoc!
+       'dict-assoc!
        "uneven number of expressions. kv-args must come in <key> <val> pairs."
        "kv-args" kv-args))
     (begin (apply dict-set*! d kv-args) d)]))
 
-(def dissoc
+(def dict-dissoc
   (case-fn
    [(d k) (dict-remove d k)]
    [(d & ks)
@@ -52,13 +64,13 @@
           d
           (loop (rest ks) (dict-remove d (first ks)))))]))
 
-(def dissoc!
+(def dict-dissoc!
   (case-fn
    [(d k) (begin (dict-remove! d k) d)]
    [(d & ks)
     (begin (for ([k ks]) (dict-remove! d k)) d)]))
 
-;; TODO - test assoc using wrong num of args - err out (check contract)
+;; TODO - test dict-assoc using wrong num of args - err out (check contract)
 
 (module+ test
   (require rackunit
@@ -66,91 +78,91 @@
            racket/hash)
 
   (test-case
-      "assoc - immutable dicts - add an entry"
+      "dict-assoc - immutable dicts - add an entry"
     (local [(def ih (make-immutable-hash '((a . 1) (d . 4))))]
            ;; add an entry
-           (let ([ret-dict (assoc ih 'e 10)])
+           (let ([ret-dict (dict-assoc ih 'e 10)])
              (check-equal? ret-dict (make-immutable-hash '((a . 1) (d . 4) (e . 10)))))
            ;; ensure modification is not in-place
            (check-equal? ih (make-immutable-hash '((a . 1) (d . 4))))))
 
   (test-case
-      "assoc - immutable dicts - overwrite an entry"
+      "dict-assoc - immutable dicts - overwrite an entry"
     (local [(def ih (make-immutable-hash '((a . 1) (d . 4))))]
            ;; add an entry
-           (let ([ret-dict (assoc ih 'a 10)])
+           (let ([ret-dict (dict-assoc ih 'a 10)])
              (check-equal? ret-dict (make-immutable-hash '((a . 10) (d . 4)))))
            ;; ensure modification is not in-place
            (check-equal? ih (make-immutable-hash '((a . 1) (d . 4))))))
 
   (test-case
-      "assoc - immutable dicts - add several entries"
+      "dict-assoc - immutable dicts - add several entries"
     (local [(def ih (make-immutable-hash '((a . 1) (d . 4))))]
            ;; add several entries
-           (let ([ret-dict (assoc ih 'd 7 'e 10 'f 25)])
+           (let ([ret-dict (dict-assoc ih 'd 7 'e 10 'f 25)])
              (check-equal? ret-dict (make-immutable-hash '((a . 1) (d . 7) (e . 10) (f . 25))))
              ;; ensure modification is in-place
              (check-equal? ih (make-immutable-hash '((a . 1) (d . 4)))))))
 
   (test-case
-      "dissoc - immutable dicts - remove an entry"
+      "dict-dissoc - immutable dicts - remove an entry"
     (local [(def ih (make-immutable-hash '((a . 1) (c . 3) (d . 4))))]
            ;; remove an entry
-           (let ([ret-dict (dissoc ih 'd)])
+           (let ([ret-dict (dict-dissoc ih 'd)])
              (check-equal? ret-dict (make-immutable-hash '((a . 1) (c . 3)))))
            ;; ensure modification is not in-place
            (check-equal? ih (make-immutable-hash '((a . 1) (c . 3) (d . 4))))))
 
   (test-case
-      "dissoc - immutable dicts - remove several entries"
+      "dict-dissoc - immutable dicts - remove several entries"
     (local [(def ih (make-immutable-hash '((a . 1) (b . 2) (c . 3) (d . 4) (e . 5) (f . 6))))]
            ;; remove several entries
-           (let ([ret-dict (dissoc ih 'b 'e 'f)])
+           (let ([ret-dict (dict-dissoc ih 'b 'e 'f)])
              (check-equal? ret-dict (make-immutable-hash '((a . 1) (c . 3) (d . 4))))
              ;; ensure modification is in-place
              (check-equal? ih (make-immutable-hash '((a . 1) (b . 2) (c . 3) (d . 4) (e . 5) (f . 6)))))))
   
   (test-case
-      "assoc - mutable dicts - add an entry"
+      "dict-assoc - mutable dicts - add an entry"
     (local [(def mh (make-hash '((a . 1) (d . 4))))]
            ;; add an entry
-           (let ([ret-dict (assoc! mh 'e 10)])
+           (let ([ret-dict (dict-assoc! mh 'e 10)])
              (check-equal? ret-dict (make-hash '((a . 1) (d . 4) (e . 10))))
              ;; ensure modification is in-place
              (check-equal? mh ret-dict))))
 
   (test-case
-      "assoc - mutable dicts - overwrite an entry"
+      "dict-assoc - mutable dicts - overwrite an entry"
     (local [(def mh (make-hash '((a . 1) (d . 4))))]
            ;; overwrite an entry
-           (let ([ret-dict (assoc! mh 'a 10)])
+           (let ([ret-dict (dict-assoc! mh 'a 10)])
              (check-equal? ret-dict (make-hash '((a . 10) (d . 4))))
              ;; ensure modification is in-place
              (check-equal? mh ret-dict))))
 
   (test-case
-      "assoc - mutable dicts - add several entries"
+      "dict-assoc - mutable dicts - add several entries"
     (local [(def mh (make-hash '((a . 1) (d . 4))))]
            ;; add several entries
-           (let ([ret-dict (assoc! mh 'd 7 'e 10 'f 25)])
+           (let ([ret-dict (dict-assoc! mh 'd 7 'e 10 'f 25)])
              (check-equal? ret-dict (make-hash '((a . 1) (d . 7) (e . 10) (f . 25))))
              ;; ensure modification is in-place
              (check-equal? mh ret-dict))))
 
   (test-case
-      "dissoc - mutable dicts - remove an entry"
+      "dict-dissoc - mutable dicts - remove an entry"
     (local [(def mh (make-hash '((a . 1) (c . 3 ) (d . 4))))]
            ;; remove an entry
-           (let ([ret-dict (dissoc! mh 'd)])
+           (let ([ret-dict (dict-dissoc! mh 'd)])
              (check-equal? ret-dict (make-hash '((a . 1) (c . 3))))
              ;; ensure modification is in-place
              (check-equal? mh ret-dict))))
 
   (test-case
-      "dissoc - mutable dicts - remove several entries"
+      "dict-dissoc - mutable dicts - remove several entries"
     (local [(def mh (make-hash '((a . 1) (b . 2) (c . 3) (d . 4) (e . 5) (f . 6))))]
            ;; remove several entries
-           (let ([ret-dict (dissoc! mh 'b 'e 'f)])
+           (let ([ret-dict (dict-dissoc! mh 'b 'e 'f)])
              (check-equal? ret-dict (make-hash '((a . 1) (c . 3) (d . 4))))
              ;; ensure modification is in-place
              (check-equal? mh ret-dict)))))
