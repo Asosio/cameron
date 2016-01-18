48a922f0cadbbcd4f8cb9798c9bf0bb34e8d0258
Jesper Wendel Devantier <j@asosio.com>
2016-01-18 23:30:33 +0100

    restructure main

--- main.rkt
+++ main.rkt
@@ -1,11 +1,29 @@
 #lang racket
 (require "defs.rkt"
-         "hashtable.rkt"
-         threading)
+         threading
+         "collections/dict.rkt"
+         "collections/set.rkt"
+         "collections/has.rkt"
+         "collections/countable.rkt"
+         "collections/frozen.rkt"
+         "collections/key-get.rkt"
+         "collections/key-set.rkt"
+         "collections/key-rm.rkt"
+         "collections/nth-get.rkt"
+         "collections/nth-set.rkt")
 (provide
  (except-out (all-from-out racket)
              define
              lambda)
  (all-from-out "defs.rkt"
-               threading)
- (prefix-out ht/ (all-from-out "hashtable.rkt")))
+               threading
+               "collections/dict.rkt"
+               "collections/set.rkt"
+               "collections/has.rkt"
+               "collections/countable.rkt"
+               "collections/frozen.rkt"
+               "collections/key-get.rkt"
+               "collections/key-set.rkt"
+               "collections/key-rm.rkt"
+               "collections/nth-get.rkt"
+               "collections/nth-set.rkt"))
